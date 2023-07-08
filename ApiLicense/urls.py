4879ac8867417e541bd261636c2b7008af573dcb
AngelGaboCC <angelg.chablec@gmail.com>
2023-07-08 14:47:14 -0600

    quinto commit

--- ApiLicense/urls.py
+++ ApiLicense/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ApiLicense import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('licenses/', views.license_list),
     path('licenses/<int:id>', views.license_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
